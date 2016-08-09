e0d32e5c4659d03de124c5d52c105f85f19f107a
RICHARD MONSON-HAEFEL <monsonhaefel@gmail.com>
2016-08-09 09:50:29 -0500

    Initial Commit

---
+++ MusicVideo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicVideo
+//
+//  Created by RICHARD MONSON-HAEFEL on 8/9/16.
+//  Copyright © 2016 Uncommon Design, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
