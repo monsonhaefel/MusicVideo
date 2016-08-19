7cdd7fcd9334f5055bdc5aeb3d8ff28c38057227
RICHARD MONSON-HAEFEL <monsonhaefel@gmail.com>
2016-08-19 10:56:13 -0500

    Part-24: Slider user defaults and About page

--- MusicVideo/SettingsTVC.swift
+++ MusicVideo/SettingsTVC.swift
@@ -31,9 +31,23 @@ class SettingsTVC: UITableViewController {
         title = "Settings"
         
         touchId.on = NSUserDefaults.standardUserDefaults().boolForKey("SecSetting")
+        
+        if(NSUserDefaults.standardUserDefaults().floatForKey("APICNT") != Float(0)){
+            let theValue = NSUserDefaults.standardUserDefaults().objectForKey("APICNT") as! Int
+            APICnt.text = "\(theValue)"
+            sliderCnt.value = Float(theValue)
+            
+        }
+        
 
     }
     
+    @IBAction func valueChanged(sender: UISlider) {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(Int(sliderCnt.value), forKey: "APICNT")
+        APICnt.text = ("\(Int(sliderCnt.value))")
+        
+    }
 
     @IBAction func touchIdSec(sender: UISwitch) {
         
